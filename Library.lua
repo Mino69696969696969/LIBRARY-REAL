48cce951e199f8e000db6223a9b5b30e53f0439c
Mino69696969696969 <105103231+Mino69696969696969@users.noreply.github.com>
2022-12-22 23:31:53 +0100

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -31,7 +31,7 @@ local Library = {
     FontColor = Color3.fromRGB(255, 255, 255);
     MainColor = Color3.fromRGB(28, 28, 28);
     BackgroundColor = Color3.fromRGB(20, 20, 20);
-    AccentColor = Color3.fromRGB(199,21,133);
+    AccentColor = Color3.fromRGB(102,51,153);
     OutlineColor = Color3.fromRGB(50, 50, 50);
 
     Black = Color3.new(0, 0, 0);
